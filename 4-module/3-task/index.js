38c595524f9361be79898ea2b89a2bea1261f692
Ruslan Saifutdinov <ftadsh@gmail.com>
2023-05-25 14:38:28 +0500

    Решение заданий из урока №3 с 1 по 3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.rows;
+  
+  for(let i = 1; i < rows.length; i++) {
+    let rowItem = rows[i].cells;
+    
+    if(Number(rowItem[1].textContent) < 18 ) {
+      rows[i].style.textDecoration = 'line-through';
+    };
+    
+    if(rowItem[2].textContent === 'm') {
+      rows[i].classList.add('male');
+    } else if(rowItem[2].textContent === 'f') {
+      rows[i].classList.add('female');
+    };
+  
+    if (rowItem[3].dataset.available === 'true') {
+      rows[i].classList.add('available');
+    } else if (rowItem[3].dataset.available === 'false') {
+      rows[i].classList.add('unavailable');
+    } else {
+      rows[i].setAttribute("hidden", '');
+    }
+  };
+
 }
