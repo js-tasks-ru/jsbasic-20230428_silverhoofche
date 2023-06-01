4ceac100fae77ca86b82ac15cba6198627423c46
Ruslan Saifutdinov <ftadsh@gmail.com>
2023-06-01 18:56:29 +0500

    Решение заданий 6 модуля с 1 по 3

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,44 @@
  *
  */
 export default class UserTable {
+  elem = null; 
+  #arr = '';
+
+
   constructor(rows) {
+    this.#arr = rows;
+    this.elem = this.#render();
   }
+
+  #render() {
+    let element = document.createElement('tbody');
+    element.innerHTML = this.#template();
+    element.addEventListener('click', this.#onCloseClick);
+    return element;
+  }
+
+  #template() {
+    return (
+      this.#arr.map(item => {
+        return (
+          `<tr>
+            <td>${item.name}</td>
+            <td>${item.age}</td>
+            <td>${item.salary}</td>
+            <td>${item.city}</td>
+            <td><button data-action='remove'>X</button></td>
+          </tr>`);
+      }).join('\n')
+    );
+  }
+
+  #onCloseClick = (event) => {
+    if(event.target.dataset.action !== 'remove') {
+      return
+    }
+
+    let removeButton = event.target;
+    removeButton.closest('tr').remove();
+  };
+
 }
